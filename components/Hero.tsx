857f8e095e100747f6ed5949dbd52207196b4882
Rinmacarion <manuel.ging@gmail.com>
2025-11-30 18:38:49 -0500

    fixed image issue

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { ArrowDown, Mail } from 'lucide-react';
 import { PERSONAL_INFO } from '../constants';
 import { Link } from 'react-router-dom';
-import nadjaProfile from '/assets/nadja.png';
+import nadjaProfile from '/nadja.png';
 
 const Hero: React.FC = () => {
 
