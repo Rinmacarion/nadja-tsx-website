3fe087bbdcf70a101d6ee0266a4bcecc7e57860d
Rinmacarion <manuel.ging@gmail.com>
2026-02-08 16:13:31 -0500

    Fix punctuation in Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,1 @@
-export const Hero = () => {
-    return (
-        <section>
-            <h1>Mission</h1>
-            <p>
-                I lead an independent research program on how humans and machines adapt in uncertain, dynamic environments. Bridging neuroscience, psychiatry, AI, and economics, my work focuses on meta-learning, behavioral adaptability, and perceived controllability - to generate interdisciplinary insights & build scalable ecosystems for scientific discovery and real-world impact.
-            </p>
-        </section>
-    );
-};
+How minds, biological and artificial, navigate uncertainty and change
