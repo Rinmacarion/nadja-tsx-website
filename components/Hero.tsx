9703f623f674d1146c38cbfb1318a7f142b51f66
Rinmacarion <manuel.ging@gmail.com>
2026-04-14 19:43:51 -0400

    changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -77,7 +77,7 @@ const Hero: React.FC = () => {
           </div>
           <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start mb-12">
             <a
-              href="/assets/CV_GingJehli_040826.pdf"
+              href="/assets/CV_GingJehli_041326.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="px-6 sm:px-12 w-full sm:w-72 py-3 bg-academic-200 text-academic-800 rounded-lg font-medium hover:bg-academic-300 transition-colors shadow-lg hover:shadow-xl flex items-center justify-center gap-2"
